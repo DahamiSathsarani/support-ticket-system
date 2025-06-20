9fc82e82ea9278eb4ea4b7039b1a8aaaf4327213
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2025-06-20 16:03:30 +0530

    Create user registration & jwt login

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"github.com/DahamiSathsarani/support-ticket-system/database"
 	"github.com/gin-gonic/gin"
+	"github.com/DahamiSathsarani/support-ticket-system/database"
+	"github.com/DahamiSathsarani/support-ticket-system/models"
+	"github.com/DahamiSathsarani/support-ticket-system/routes"
 )
 
 func main() {
 	r := gin.Default()
 
 	database.Connect()
+	database.DB.AutoMigrate(&models.User{})
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
-	})
+	routes.AuthRoutes(r)
 
 	r.Run(":8080")
 }
